fab78cdb1d9fab2254692146e75535ccc7a1531f
dasankim <106504779+DasanKim@users.noreply.github.com>
2023-11-07 01:26:12 +0900

    [Silver III] Title: 두 수의 합, Time: 48 ms, Memory: 76884 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.swift
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.swift
@@ -1,19 +1,27 @@
 let n = readLine()!
-let numbers = readLine()!.split(separator: " ").compactMap { Int($0) }
+let numbers = readLine()!.split(separator: " ").compactMap { Int($0) }.sorted()
 let x = Int(readLine()!)!
 
-var array = Array(repeating: 0, count: x)
 var result = 0
+var s = 0
+var e = numbers.count - 1
 
-for i in numbers {
-    if x <= i { continue }
+for _ in 0..<numbers.count {
+    let sum = numbers[s] + numbers[e]
 
-    let pair = x - i
+    if s >= e {
+        break
+    }
 
-    if array[pair - 1] == 1 {
+    if sum > x {
+        e -= 1
+    } else if sum < x {
+        s += 1
+    } else if sum == x {
+        s += 1
+        e -= 1
         result += 1
     }
-    array[i - 1] = 1
 }
 
 print(result)
