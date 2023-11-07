96212afa0120ad51de43de441c61b3bdb0ffc27e
dasankim <106504779+DasanKim@users.noreply.github.com>
2023-11-07 15:42:56 +0900

    [level unrated] Title: 문자열 여러 번 뒤집기, Time: 64.39 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.swift
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.swift
@@ -1,14 +1,18 @@
 import Foundation
 
 func solution(_ my_string:String, _ queries:[[Int]]) -> String {
-    var str = my_string
+//     var str = my_string
     
-    for q in queries {
-        let range = str.index(str.startIndex, offsetBy: q[0])...str.index(str.startIndex, offsetBy: q[1])
-        let subAarray = str[range].reversed()
-        str.removeSubrange(range)
-        str.insert(contentsOf: subAarray, at: str.index(str.startIndex, offsetBy: q[0]))
-    }
+//     for q in queries {
+//         let range = str.index(str.startIndex, offsetBy: q[0])...str.index(str.startIndex, offsetBy: q[1])
+//         let subAarray = str[range].reversed()
+//         str.removeSubrange(range)
+//         str.insert(contentsOf: subAarray, at: str.index(str.startIndex, offsetBy: q[0]))
+//     }
+    
+    var str = my_string.map { String($0) }
+    
+    queries.forEach { str[$0[0]...$0[1]].reverse() }
 
-    return str
+    return str.joined()
 }
