9b727c30c46e3bf035d88fdac729bd69b6db8222
dasankim <106504779+DasanKim@users.noreply.github.com>
2023-11-20 21:09:35 +0900

    [Silver IV] Title: 카드2, Time: 32 ms, Memory: 73008 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.swift
@@ -0,0 +1,40 @@
+let n = Int(readLine()!)!
+var head = 0
+var tail = 0
+var isEmpty: Bool {
+    head == tail
+}
+
+var arr = Array(repeating: 0, count: n)
+
+for i in 1...n {
+    push(i)
+}
+
+for _ in 0..<n {
+    _ = pop()
+    let num = pop()
+
+    if isEmpty {
+        print(num)
+        break
+    }
+
+    push(num)
+}
+
+func push(_ num: Int) {
+    arr[tail%n] = num
+    tail += 1
+}
+
+func pop() -> Int {
+    var num = 1
+
+    if head != tail {
+        num = arr[head%n]
+        head += 1
+    }
+
+    return num
+}
