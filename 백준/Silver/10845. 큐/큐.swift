d0430553f4999a4e59ec5c064ed678f7d9729f25
dasankim <106504779+DasanKim@users.noreply.github.com>
2023-11-20 18:34:17 +0900

    [Silver IV] Title: 큐, Time: 24 ms, Memory: 69564 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.swift
+++ 백준/Silver/10845. 큐/큐.swift
@@ -26,9 +26,9 @@ func queue(_ command: [String]) {
         case "empty":
         result.append(tail == head ? "1" : "0")
         case "front":
-        result.append((arr.isEmpty || arr[head%n] == "") ? "-1" : arr[head%n])
+        result.append(arr[head%n] == "" ? "-1" : arr[head%n])
         case "back":
-        result.append((arr.isEmpty || head == tail) ? "-1" : arr[tail%n-1])
+        result.append(head == tail ? "-1" : arr[tail%n-1])
         default:
         print("error")
     }
