2036d38f3d7ba50d998a97a9ae3107daba3b0fa6
Dasan <106504779+DasanKim@users.noreply.github.com>
2023-12-10 13:08:37 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2120 ms, Memory: 81724 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.swift
@@ -0,0 +1,38 @@
+let caseCount = Int(readLine()!)!
+var arr: [Int] = []
+var vist: [Bool] = []
+var count = 0
+
+for _ in 0..<caseCount {
+    let sn = Int(readLine()!)!
+    arr = readLine()!.split(separator: " ").compactMap { Int($0)! - 1 }
+    vist = Array(repeating: false, count: sn)
+    count = 0
+
+    for i in 0..<sn {
+        if vist[i] { continue }
+        dfs(i)
+    }
+    print(count)
+}
+
+func dfs(_ n: Int) {
+    var tempVist: [Int] = []
+    var stack:[Int] = [n]
+
+    while !stack.isEmpty {
+        let cur = stack.popLast()!
+        let next = arr[cur]
+        vist[cur] = true
+        tempVist.append(cur)
+
+        if !vist[next] {
+            stack.append(next)
+        } else {
+            for i in tempVist {
+                if i == next { break }
+                count += 1
+            }
+        }
+    }
+}
