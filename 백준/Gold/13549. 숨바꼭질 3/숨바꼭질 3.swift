55b81e11bf21915752435b701516344aab5f3559
Dasan <106504779+DasanKim@users.noreply.github.com>
2023-12-11 23:10:39 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 16 ms, Memory: 71456 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.swift
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.swift
@@ -1,31 +1,33 @@
-let inputs = readLine()!.split(separator: " ").compactMap { Int($0) }
-var queue: [Int] = []
-let max = 100001
-var board: [Int] = Array(repeating: -1, count: max)
-
-board[inputs[0]] = 0
-queue.append(inputs[0])
-
-while !queue.isEmpty {
-    let cur = queue.removeFirst()
-
-    if cur == inputs[1] {
-        print(board[cur])
-        break
-    }
-
-    let dx = [cur*2, cur-1, cur+1]
-    for i in 0..<3 {
-        let nx = dx[i]
-
-        if !(0..<max ~= nx) || board[nx] != -1  { continue }
-
-        if i == 0 {
-            board[nx] = board[cur]
-        } else {
-            board[nx] = board[cur] + 1
+    let inputs = readLine()!.split(separator: " ").compactMap { Int($0) }
+    var queue: [Int] = []
+    let max = 100001
+    var board: [Int] = Array(repeating: -1, count: max)
+    
+    board[inputs[0]] = 0
+    queue.append(inputs[0])
+    
+    var pointer = 0
+    while pointer < queue.count {
+        let cur = queue[pointer]
+        pointer += 1
+        
+        if cur == inputs[1] {
+            print(board[cur])
+            break
         }
-
-        queue.append(nx)
+        
+        let dx = [cur*2, cur-1, cur+1]
+        for i in 0..<3 {
+            let nx = dx[i]
+            
+            if !(0..<max ~= nx) || board[nx] != -1  { continue }
+            
+            if i == 0 {
+                board[nx] = board[cur]
+            } else {
+                board[nx] = board[cur] + 1
+            }
+            
+            queue.append(nx)
+        }
     }
-}
