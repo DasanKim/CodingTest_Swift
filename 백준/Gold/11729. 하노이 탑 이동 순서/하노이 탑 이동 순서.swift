89802b1185d215a7a1d284156906aed7f38be543
Dasan <106504779+DasanKim@users.noreply.github.com>
2023-12-14 10:22:29 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 576 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let k = Int(readLine()!)!
+
+print(pow(2, k)-1)
+hanoi(start: 1, dest: 3, n: k)
+
+func hanoi(start: Int, dest: Int, n: Int) {
+    if n == 1 {
+        print("\(start) \(dest)")
+        return
+    }
+
+    hanoi(start: start, dest: 6-start-dest, n: n-1)
+
+    print("\(start) \(dest)")
+
+    hanoi(start: 6-start-dest, dest: dest, n: n-1)
+}
