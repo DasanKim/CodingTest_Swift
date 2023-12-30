e507c825a068bde7bd2b9fc56b894128bbcd518d
Dasan <106504779+DasanKim@users.noreply.github.com>
2023-12-30 19:26:06 +0900

    [Silver II] Title: N과 M (10), Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.swift
@@ -0,0 +1,25 @@
+let inputs = readLine()!.split(separator: " ").compactMap { Int($0) }
+let numbers = readLine()!.split(separator: " ").compactMap({ Int($0) }).sorted()
+
+let n = inputs[0]
+let m = inputs[1]
+
+var result = ""
+process(0, index: 0, seq: "")
+print(result)
+
+func process(_ x: Int, index: Int, seq: String) {
+    if x == m {
+        result += "\(seq)\n"
+        return
+    }
+
+    var preValue = 0
+
+    for i in index..<n {
+        if preValue != numbers[i] {
+            preValue = numbers[i]
+            process(x+1, index: i+1, seq: seq+"\(numbers[i]) ")
+        }
+    }
+}
