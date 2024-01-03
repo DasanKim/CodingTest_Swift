2957426f479ce184aa09d927416394af52e9a17c
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-01-03 11:49:50 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 456 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.swift
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.swift
@@ -1,40 +1,43 @@
 let n = Int(readLine()!)!
-var eggs: [(Int, Int)] = []
+var weight: [Int] = []
+var ph: [Int] = []
+
 var result = 0
 
 for _ in 0..<n {
     let input = readLine()!.split(separator: " ").compactMap { Int($0) }
-    eggs.append((input[0], input[1]))
+    ph.append(input[0])
+    weight.append(input[1])
 }
 
-process(0)
+process2(0)
 print(result)
 
-func process(_ x: Int) { // x: 현재 들고 있는 계란
+func process2(_ x: Int) { // x: 현재 들고 있는 계란
     if x == n {
         return
     }
-    
+
     for i in 0..<n {
-        if eggs[x].0 <= 0 { // 내 계란이 깨져있으면 x+1로 넘어가기
-            process(x+1)
+        if ph[x] <= 0 { // 내 계란이 깨져있으면 x+1로 넘어가기
+            process2(x+1)
         } else { // 부딪히려는 계란이 깨져있으면 다음으로 넘어가기
-            if x != i && eggs[i].0 > 0 {
-                eggs[x].0 -= eggs[i].1
-                eggs[i].0 -= eggs[x].1
-                count(eggs)
-                process(x+1)
-                eggs[x].0 += eggs[i].1
-                eggs[i].0 += eggs[x].1
+            if x != i && ph[i] > 0 {
+                ph[x] -= weight[i]
+                ph[i] -= weight[x]
+                count2(ph)
+                process2(x+1)
+                ph[x] += weight[i]
+                ph[i] += weight[x]
             }
         }
     }
 }
 
-func count(_ eggs: [(Int, Int)]) {
+func count2(_ ph: [Int]) {
     var count = 0
-    for egg in eggs {
-        if egg.0 <= 0 { count += 1 }
+    for egg in ph {
+        if  egg <= 0 { count += 1 }
     }
     result = max(result, count)
 }
