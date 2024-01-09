e48c5bf00f0e180d675b7fe9012d70f723585a64
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-01-09 09:30:38 +0900

    [Gold IV] Title: 감시, Time: 156 ms, Memory: 76792 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.swift
+++ 백준/Gold/15683. 감시/감시.swift
@@ -18,12 +18,14 @@ var vist = Array(repeating: Array(repeating: false, count: m), count: n)
 var combi: [Int] = []
 var combis: [[Int]] = []
 var result = 9999
+var wallCount = 0
 
 // 1. cctv 좌표 확인
 for i in 0..<n {
     let input = readLine()!.split(separator: " ").compactMap { Int($0) }
     for j in 0..<m {
         if 1...5 ~= input[j] { cctv.append((i, j)) }
+        if input[j] == 6 { wallCount += 1 }
         board[i][j] = input[j]
     }
 }
@@ -38,6 +40,7 @@ func process() {
 
     for i in 0..<combis.count {
         vist = Array(repeating: Array(repeating: false, count: m), count: n)
+        var count = 0
 
         for j in 0..<combis[i].count {
             let curDir = combis[i][j]
@@ -45,25 +48,27 @@ func process() {
             let curCctv = board[curCoord.0][curCoord.1]
 
             if  curCctv == 1 {
-                straight(curCoord, dir: curDir)
+                count += straight(curCoord, dir: curDir)
             } else if curCctv == 2 {
-                straight(curCoord, dir: curDir)
-                straight(curCoord, dir: curDir+2)
+                count += straight(curCoord, dir: curDir)
+                count += straight(curCoord, dir: curDir+2)
             } else if curCctv == 3 {
-                straight(curCoord, dir: curDir)
-                straight(curCoord, dir: curDir+1)
+                count += straight(curCoord, dir: curDir)
+                count += straight(curCoord, dir: curDir+1)
             } else if curCctv == 4 {
-                straight(curCoord, dir: curDir)
-                straight(curCoord, dir: curDir+1)
-                straight(curCoord, dir: curDir+3)
+                count += straight(curCoord, dir: curDir)
+                count += straight(curCoord, dir: curDir+1)
+                count += straight(curCoord, dir: curDir+3)
             } else if curCctv == 5 {
-                straight(curCoord, dir: curDir)
-                straight(curCoord, dir: curDir+1)
-                straight(curCoord, dir: curDir+2)
-                straight(curCoord, dir: curDir+3)
+                count += straight(curCoord, dir: curDir)
+                count += straight(curCoord, dir: curDir+1)
+                count += straight(curCoord, dir: curDir+2)
+                count += straight(curCoord, dir: curDir+3)
             }
         }
-        result = min(check(), result)
+
+        let test = (n*m) - (count + wallCount + cctv.count)
+        result = min(test, result)
     }
 }
 
@@ -84,32 +89,23 @@ func search(_ x: Int) {
 }
 
 // 4. 현재 좌표에서 특정 방향으로 직진!
-func straight(_ coord: (Int, Int), dir: Int) {
+func straight(_ coord: (Int, Int), dir: Int) -> Int {
     var nx = coord.0
     var ny = coord.1
     let curDir = dir % 4
+    var count = 0
 
     while true {
         nx += dx[curDir]
         ny += dy[curDir]
 
-        if !(0..<n ~= nx) || !(0..<m ~= ny) { return }
-        if board[nx][ny] == 6 { return }
-        if vist[nx][ny] == true { continue }
+        if !(0..<n ~= nx) || !(0..<m ~= ny) { break }
+        if board[nx][ny] == 6 { break }
+        if (1...5 ~= board[nx][ny]) || vist[nx][ny] == true { continue }
 
         vist[nx][ny] = true
+        count += 1
     }
-}
 
-// 5. 사각지대 체크
-func check() -> Int {
-    var count = 0
-
-    for i in 0..<n {
-        for j in 0..<m {
-            if (1...6 ~= board[i][j]) || vist[i][j] == true { continue }
-            count += 1
-        }
-    }
     return count
 }
