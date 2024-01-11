2e9ea0b68716db40bd0ab685326e4ffa71d04edc
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-01-11 21:57:23 +0900

    [Silver V] Title: 단어 정렬, Time: 56 ms, Memory: 75640 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.swift
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.swift
@@ -1,14 +1,23 @@
 let n = Int(readLine()!)!
-var str = Set<String>()
+var words = Set<String>()
+var sortedWords = [(String, Int)]()
 var result = ""
 
-for _ in 0..<n { str.insert(readLine()!) }
-
-let storedWords = str.sorted { a, b in
-    (a.count != b.count) ? a.count < b.count : a < b
-    }
-
-for word in storedWords {
-    result += "\(word)\n"
+for _ in 0..<n {
+    let input = readLine()!
+    words.insert(input)
 }
+
+for word in words {
+    sortedWords.append((word, word.count))
+}
+
+let temp = sortedWords.sorted { a, b in
+ (a.1 != b.1) ? a.1 < b.1 : a.0 < b.0
+}
+
+for i in temp {
+    result += "\(i.0)\n"
+}
+
 print(result)
