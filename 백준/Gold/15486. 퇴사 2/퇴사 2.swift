e769ed8c6dab1108c886092b9ff7b9f5147bc49b
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-01-23 10:30:26 +0900

    [Gold V] Title: 퇴사 2, Time: 1820 ms, Memory: 130756 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.swift
@@ -0,0 +1,22 @@
+let n = Int(readLine()!)!
+var arr: [(Int, Int)] = []
+var dp = Array(repeating: 0, count: n+1)
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    arr.append((input[0], input[1]))
+}
+
+for i in stride(from: 0, to: n, by: 1) {
+    let index = i + arr[i].0
+
+    if dp[i] > dp[i+1] {
+        dp[i+1] = dp[i]
+    }
+
+    if index <= n {
+        dp[index] = max(dp[index], dp[i] + arr[i].1)
+    }
+}
+
+print(dp[n])
