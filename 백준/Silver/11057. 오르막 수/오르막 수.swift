fc125574b4330dd0cec468830161c899b13d370a
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-02-02 16:26:51 +0900

    [Silver I] Title: 오르막 수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.swift
+++ 백준/Silver/11057. 오르막 수/오르막 수.swift
@@ -1,18 +1,11 @@
 let n = Int(readLine()!)!
-var dp = Array(repeating: Array(repeating: 0, count: 11), count: n+1)
-dp[1] = [1,  1,  1,  1,  1,  1,  1, 1, 1, 1, 0]
+var dp = Array(repeating: 1, count: 10)
 
-if n > 1 {
-    for i in 2...n {
-        for j in (0..<10).reversed() {
-            dp[i][j] = (dp[i-1][j] + dp[i][j+1]) % 10007
-        }
+for _ in 0..<n {
+    for i in 1...9 {
+        dp[i] += dp[i-1]
+        dp[i] %= 10007
     }
 }
 
-var result = 0
-for arr in dp[n] {
-    result += arr
-}
-
-print(result%10007)
+print(dp[9])
