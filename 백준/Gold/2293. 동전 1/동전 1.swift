6a5002beb29b8bf7d6af73065ef5a86e50994b17
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-02-05 16:19:01 +0900

    [Gold V] Title: 동전 1, Time: 28 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.swift
+++ 백준/Gold/2293. 동전 1/동전 1.swift
@@ -1,24 +1,24 @@
 import Foundation
-let nk = readLine()!.split(separator: " ").map{Int(String($0))!}
-let n = nk[0]
-let k = nk[1]
 
-var coin = [Int]()
-for _ in 0..<n{
-    coin.append(Int(String(readLine()!))!)
+let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+let n = input[0]
+let k = input[1]
+var arr: [Int] = []
+var dp = Array(repeating: 0, count: k+1)
+
+for _ in 0..<n {
+    arr.append(Int(readLine()!)!)
 }
-var dp = Array(repeating: 0, count: k + 1)
-dp[0] = 1
 
-for i in 0..<n{
-    for j in stride(from: coin[i], through: k, by: 1){
-        
-        if dp[j] + dp[j - coin[i]] >= Int(pow(2.0, 31.0)){
+dp[0] = 1 
+
+for i in 0..<n {
+    for j in stride(from: arr[i], through: k, by: 1) {
+        dp[j] = dp[j] + dp[j-arr[i]]
+        if  dp[j] >= Int(pow(2.0, 31.0)) {
             dp[j] = 0
-        }else{
-            dp[j] += dp[j - coin[i]]
         }
     }
-}
 
+}
 print(dp[k])
