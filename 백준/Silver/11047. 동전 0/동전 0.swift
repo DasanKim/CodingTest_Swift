db0b7f425bf3544f870720d52691fdd1a30d732b
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-02-13 21:25:04 +0900

    [Silver IV] Title: 동전 0, Time: 188 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.swift
@@ -0,0 +1,23 @@
+let inputs = readLine()!.split(separator: " ").compactMap{ Int($0) }
+let n = inputs[0]
+var k = inputs[1]
+var arr: [Int] = [0]
+
+for _ in 0..<n {
+    arr.append(Int(readLine()!)!)
+}
+
+var index = n
+var coinCount = 0
+
+while index != 0 && k != 0 {
+    if arr[index] > k {
+        index -= 1
+        continue
+    } else {
+        k -= arr[index]
+        coinCount += 1
+    }
+}
+
+print(coinCount)
