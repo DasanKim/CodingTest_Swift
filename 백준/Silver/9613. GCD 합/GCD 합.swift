a4f68c194c8985108fed9f69d572b3069a42a18c
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-03-01 00:09:59 +0900

    [Silver IV] Title: GCD 합, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.swift
+++ 백준/Silver/9613. GCD 합/GCD 합.swift
@@ -1,15 +1,13 @@
 let t = Int(readLine()!)!
 
 for _ in 0..<t {
-    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
-    let n = input[0]
-    let arr = input[1...input.count-1].sorted(by: >)
+    var input = readLine()!.split(separator: " ").compactMap { Int($0) }
+    let n = input.removeFirst()
     var result = 0
 
     for i in 0..<n {
-        for j in i..<n {
-            if i == j { continue }
-            result += gcd(a: arr[i], b: arr[j])
+        for j in i+1..<n {
+            result += gcd(a: input[i], b: input[j])
         }
     }
     print(result)
