8386bd63bfdae8efe7bc42b79b4fcaea38f38efb
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-03-01 10:46:38 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.swift
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.swift
@@ -1,11 +1,9 @@
-import Foundation
-
 let input = readLine()!.split(separator: " ").compactMap { Int($0) }
 let up = input[0]
 let down = input[1]
 let goal = input[2]
 
 let sum = up - down
-let day = ceil(Double(goal - down) / Double(sum))
+let day = (goal - down - 1) / sum + 1
 
-print(Int(day))
+print(day)
