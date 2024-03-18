97d70e65d2f2923aafb4f107b991bc5392e0903a
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-03-18 22:03:56 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 204 ms, Memory: 89796 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.swift
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.swift
@@ -1,22 +1,22 @@
 let input = readLine()!.split(separator: " ").compactMap { Int($0) }
 let n = input[0]
 let m = input[1]
-var poketmonPedia1 = [Int:String]()
+var poketmonPedia1 = Array(repeating: "", count: n+1)//[Int:String]()
 var poketmonPedia2 = [String:Int]()
 var result = ""
 
 for i in 1...n {
-    let temp = readLine()!
-    poketmonPedia1[i] = temp
-    poketmonPedia2[temp] = i
+    let pocketmon = readLine()!
+    poketmonPedia1[i] = pocketmon
+    poketmonPedia2[pocketmon] = i
 }
 
 for _ in 0..<m {
     let q = readLine()!
     if let index = Int(q) {
-        result += "\(poketmonPedia1[index]!)\n"
+        result.write(poketmonPedia1[index] + "\n")
     } else {
-        result += "\(poketmonPedia2[q]!)\n"
+        result.write("\(poketmonPedia2[q]!)\n")
     }
 }
 
