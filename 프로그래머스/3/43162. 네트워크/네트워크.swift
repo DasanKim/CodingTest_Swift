9177e0765262a58b4fda81220e1df2efcf37f80f
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-04-28 00:04:19 +0900

    [level 3] Title: 네트워크, Time: 0.67 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -3,20 +3,22 @@ import Foundation
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     var visit = Array(repeating: false, count: n)
     var count = 0
-    var graph = Array(repeating: [Int](), count: n)
+//     var graph = Array(repeating: [Int](), count: n)
     
-    for i in 0..<n {
-        for j in 0..<n {
-            if computers[i][j] == 1 && i != j {
-                graph[i].append(j)
-            }
-        }
-    }
+//     for i in 0..<n {
+//         for j in 0..<n {
+//             if computers[i][j] == 1 && i != j {
+//                 graph[i].append(j)
+//             }
+//         }
+//     }
     
     func dfs(start: Int) {
         visit[start] = true
-        for next in graph[start] where !visit[next] {
-            dfs(start: next)
+        
+        for (i, v) in computers[start].enumerated() where !visit[i] {
+            if v != 1 { continue }
+            dfs(start: i)
         }
     }
     
