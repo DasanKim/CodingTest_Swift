0d5e0aeec6c924e2ded0a83392d99fb7be4616af
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-05-02 17:48:26 +0900

    [Gold V] Title: 트리와 쿼리, Time: 264 ms, Memory: 89388 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.swift
@@ -0,0 +1,35 @@
+let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+let n = input[0]
+let r = input[1]
+let q = input[2]
+
+var arr = Array(repeating: [Int](), count: n+1)
+var size = Array(repeating: 0, count: n+1)
+var visit = Array(repeating: false, count: n+1)
+
+for _ in 0..<n-1 {
+    let uv = readLine()!.split(separator: " ").compactMap { Int($0) }
+    arr[uv[0]].append(uv[1])
+    arr[uv[1]].append(uv[0])
+}
+
+_ = dfs(cur: r)
+
+var result = ""
+
+for _ in 0..<q {
+    let query = Int(readLine()!)!
+    result += "\(size[query])\n"
+}
+
+print(result)
+
+func dfs(cur: Int) -> Int {
+    visit[cur] = true
+    size[cur] = 1
+
+    for next in arr[cur] where !visit[next] {
+        size[cur] += dfs(cur: next)
+    }
+    return size[cur]
+}
