e7bee5b092efd050639a14e52d8a26f501a1b58a
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-05-02 23:24:06 +0900

    [Gold V] Title: 트리, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.swift
+++ 백준/Gold/1068. 트리/트리.swift
@@ -1,38 +1,29 @@
 let n = Int(readLine()!)!
 let parent = readLine()!.split(separator: " ").compactMap { Int($0) }
+let r = Int(readLine()!)!
+
 var tree = Array(repeating: [Int](), count: n)
-var visit = Array(repeating: false, count: n)
 var root = 0
 
 for (i, v) in parent.enumerated() {
-    if v != -1 {
-        tree[v].append(i)
-    } else {
+    if v == -1 {
         root = i
+    } else if i != r {
+        tree[v].append(i)
     }
 }
 
-let r = Int(readLine()!)!
 var count = 0
-
-visit[r] = true
-for i in 0..<tree.count {
-    if let index = tree[i].firstIndex(where: { $0 == r }) {
-        tree[i].remove(at: index)
-    }
-}
-
 dfs(cur: root)
-
-print(count)
+print(r == root ? 0 : count)
 
 func dfs(cur: Int) {
-    if visit[cur] { return }
+    if tree[cur].isEmpty {
+        count += 1
+        return
+    }
 
-    visit[cur] = true
-    if tree[cur].isEmpty { count += 1 }
-
-    for next in tree[cur] where !visit[next] {
+    for next in tree[cur]{
         dfs(cur: next)
     }
 }
