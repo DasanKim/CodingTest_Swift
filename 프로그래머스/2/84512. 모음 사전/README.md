ca5e30fb41e59dcacd1a2e827d965918a54f4945
Dasan <106504779+DasanKim@users.noreply.github.com>
2024-10-03 23:14:53 +0900

    [level 2] Title: 모음 사전, Time: 1.35 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 6.01 ms
+메모리: 16.5 MB, 시간: 1.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 23:01:41
+2024년 10월 03일 23:14:49
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.swift
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.swift
@@ -1,38 +1,26 @@
 import Foundation
 
-var visit: [Int] = []
 let vowel = ["A", "E", "I", "O", "U"]
+var temp: [String] = []
 var result = 0
-var temp = 1
 
 func solution(_ word:String) -> Int {
-    visit = Array(repeating: 5, count: vowel.count)
     
-    for i in 1...5 {
-        if process(str: vowel[i-1], count: 0, word: word) {
-            break
-        }
-    }
+    dfs(curr: "", word: word)
     
     return result
 }
 
-func process(str: String, count: Int, word: String) -> Bool {
-    if count == 5 { return false }
-    if str == word {
-        result = temp
-        return true
+func dfs(curr: String, word: String) {
+    if curr == word { 
+        result = temp.count
+        return 
     }
+    temp.append(curr)
     
-    temp += 1
+    if curr.count == 5 { return }
     
-    for i in 0..<vowel.count {
-        if visit[i] > 0 {
-            visit[i] -= 1
-            _ = process(str: str + vowel[i], count: count + 1, word: word)
-            visit[i] += 1
-        }
+    for i in 0..<5 {
+        dfs(curr: curr + vowel[i], word: word)
     }
-    
-    return false
 }
